2922103af8b0296bd34346c9ed8ca4fd17dc40b5
maumaumaumaumaumau <maumaumaumaumaumua@gmail.com>
2023-05-01 22:04:32 -0400

    Update video-array.js

--- video-array.js
+++ video-array.js
@@ -1,9 +1,16 @@
 const videoArray = [];
 const extensions = {"mp4": true, "mov": true, "webm": true, "3gpp": true};
+/*
 (await (await fetch(`https://api.github.com/repos/Exruw/hubvideos/git/trees/${(await (await fetch("https://api.github.com/repos/Exruw/hubvideos/commits")).json())[0]?.commit?.tree?.sha}`)).json()).tree.forEach(v => {
     if (v?.type !== "tree" && v?.path && extensions[v.path.split(".").pop()])
         videoArray.push(`https://videos.hubchicken.tk/${encodeURIComponent(v.path)}`);
-});
+});*/
+
+let files = await fetch(`https://videos.hubchicken.tk/video-array.json`)
+files = await files.json()
+
+for (const [file, url] of Object.entries(files)) videoArray.push(url)
+
 export function getArray()
 {
     return videoArray
